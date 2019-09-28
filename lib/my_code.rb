713faca4d4fe837c642e5bfd8175471159b9a4fd
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-28 15:45:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,7 @@ def reduce(source_array, starting_point = nil)
 end
 
   while i < source_array.size do 
-    total = yield(total, source_array[0])
+    total = yield(total, source_array[i])
    end
    total
 end
